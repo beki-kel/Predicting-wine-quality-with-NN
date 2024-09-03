5f3084c01bd7b40a411a3466ec5de01abeb7cbf6
Bereket <bereket.kelay@aait.edu.et>
2024-09-03 23:41:08 +0300

    chore: Refactor forward propagation method in wine_quality_nn.py and add the notbook file

--- wine-quality-prediction/train.py
+++ wine-quality-prediction/train.py
@@ -1,24 +1,75 @@
 import csv
+import random
 from wine_quality_nn import SimpleNN
 
+def normalizeData(dataset):
+    """normalize the dataset to the range [0, 1]."""
+    inputs = [row[:-1] for row in dataset]
+    minVals = [min(column) for column in zip(*inputs)]
+    maxVals = [max(column) for column in zip(*inputs)]
+
+    normalized_dataset = []
+    for row in dataset:
+        normalized_input = [(x - minVals[i]) / (maxVals[i] - minVals[i]) for i, x in enumerate(row[:-1])]
+        normalized_dataset.append(normalized_input + [row[-1]])
+    
+    return normalized_dataset, minVals, maxVals
+
+def denormalize(value, min_val, max_val):
+    """denormalize the value from [0, 1] to original range."""
+    return value * (max_val - min_val) + min_val
+
+def splitdataset(dataset, train_ratio=0.8):
+    """this splits the dataset into training and test sets"""
+    dataset = list(dataset)  # Ensure dataset is a list
+    random.shuffle(dataset)
+    split_index = int(len(dataset) * train_ratio)
+    train_data = dataset[:split_index]
+    test_data = dataset[split_index:]
+    return train_data, test_data
+
 def load_data(filepath):
-    """
-    Load the wine dataset from a CSV file.
-
-    :param filepath: Path to the CSV file containing the wine data.
-    :return: List of data rows, each containing input features followed by the target output.
-    """
+    
+    data = []
     with open(filepath, 'r') as f:
-        reader = csv.reader(f)
-        next(reader)  # Skip header
-        return list(reader)
+        reader = csv.reader(f, delimiter=';')
+        next(reader)  # Skip the header row
+        for row in reader:
+            data.append([float(value) for value in row])
+    return data
 
-if __name__ == "__main__":
-    # Load training data
-    data = load_data('winequality-red.csv')
+#loading the dataset and split to train and test
+dataset = load_data('./winequality-red.csv')
+normalized_dataset, minVals, maxVals = normalizeData(dataset)
+train_data, test_data = splitdataset(normalized_dataset, train_ratio=0.8)
 
-    # Initialize the neural network with 11 input features, 5 hidden neurons, and 1 output neuron
-    model = SimpleNN(input_size=11, hidden_size=5, output_size=1)
+#training with the data set
+nn = SimpleNN(sizeofInput=2, sizeofHidden=3, sizeofOutput=1)
+nn.train(train_data, epochs=100, learningrate=0.01)
 
-    # Train the model
-    model.train(train_data=data, epochs=100, learning_rate=0.01)
+predictions = []
+actuals = []
+
+for testRow in test_data:
+    inputData = testRow[:-1]
+    actualOutput = testRow[-1]
+    predictedoutput = nn.predict(inputData)
+    predictions.append(predictedoutput)
+    actuals.append(actualOutput)
+
+    print(f"Input: {inputData}, Actual Output: {actualOutput}, Predicted Output: {predictedoutput}")
+
+# Evaluating the prediction
+def meanSquarederror(predictions, actuals):
+    """Calculate Mean Squared Error."""
+    return sum((pred - actual) ** 2 for pred, actual in zip(predictions, actuals)) / len(predictions)
+
+def meanAbsoluteerror(predictions, actuals):
+    """Calculate Mean Absolute Error."""
+    return sum(abs(pred - actual) for pred, actual in zip(predictions, actuals)) / len(predictions)
+
+mse = meanSquarederror(predictions, actuals)
+mae = meanAbsoluteerror(predictions, actuals)
+
+print(f"Mean Squared Error: {mse}")
+print(f"Mean Absolute Error: {mae}")
--- wine-quality-prediction/wine_quality_nn.py
+++ wine-quality-prediction/wine_quality_nn.py
@@ -1,27 +1,15 @@
 import random
+
 class SimpleNN:
     def __init__(self, sizeofInput, sizeofHidden, sizeofOutput):
-
         self.sizeofHidden = sizeofHidden
         self.sizeofInput = sizeofInput
         self.sizeofOutput = sizeofOutput
 
-        self.weights_input_hidden = []
-        for i in range(sizeofInput):
-            row = []
-            for j in range(sizeofHidden):
-                row.append(random.random())
-            self.weights_input_hidden.append(row)
+        self.weights_input_hidden = [[random.random() for _ in range(sizeofHidden)] for _ in range(sizeofInput)]
+        self.bias_hidden = [random.random() for _ in range(sizeofHidden)]
 
-        self.bias_hidden= []
-        for j in range(sizeofHidden):
-            self.bias_hidden.append(random.random())
-
-        
-        self.weightsHiddenOutput=[]
-        for k in range(sizeofHidden)  :
-              self.weightsHiddenOutput.append(random.random())  
-            
+        self.weightsHiddenOutput = [random.random() for _ in range(sizeofHidden)]
         self.bias_output = random.random()
     
     """relu activation function"""
@@ -30,72 +18,55 @@ class SimpleNN:
 
     """derivative of the relu function."""
     def reluDerivative(self, x):
-        if x > 0:
-            return 1
-        else:
-            return 0
+        return 1 if x > 0 else 0
 
+    """this the func. that will do the forward pass in the network. 
+    it takes the list of inupt values and retrun output value and hidden layer output"""
     def ForwardPropagation(self, inputs):
-        """this the func. that will do the forward pass in the network. 
-        it takes the list of inupt values and retrun output value and hidden layer output"""
-        
-        # the calcs in the hidden layer
         hiddenlayerActivation = []
         for j in range(self.sizeofHidden):
-            activation = 0
-            for i in range(self.sizeofInput):
-                activation += inputs[i] * self.weights_input_hidden[i][j]
+            activation = sum(inputs[i] * self.weights_input_hidden[i][j] for i in range(self.sizeofInput))
             activation += self.bias_hidden[j]
             hiddenlayerActivation.append(activation)
 
         hiddenlayerOutput = [self.relu(x) for x in hiddenlayerActivation]
 
-        # the calcs in the output layer
-        output = 0
-        for j in range(self.sizeofHidden):
-            output += hiddenlayerOutput[j] * self.weightsHiddenOutput[j]
+        output = sum(hiddenlayerOutput[j] * self.weightsHiddenOutput[j] for j in range(self.sizeofHidden))
         output += self.bias_output
 
         return output, hiddenlayerOutput
 
-    def BacwardPropagation(self, input, hiddenLayerOutput, predictedutput, actualOutput, learningrate):
-        """Perform the BacwardPropagation pass (backpropagation) to adjust weights and biases."""
-        # Output layer error
-        outputError = predictedutput - actualOutput
+    def BackwardPropagation(self, input, hiddenLayerOutput, predicted_output, actualOutput, learningrate):
+        """ this func. will do the BacwardPropagation pass to adjust weights and biases."""
+        outputError = predicted_output - actualOutput
         d_output = outputError
 
-        # Hidden layer error
-        d_hidden_layer = [
-            d_output * self.weightsHiddenOutput[j] * self.reluDerivative(hiddenLayerOutput[j])
-            for j in range(self.sizeofHidden)
-        ]
+        d_hidden_layer = [d_output * self.weightsHiddenOutput[j] * self.reluDerivative(hiddenLayerOutput[j]) for j in range(self.sizeofHidden)]
 
-        # Update weights and biases
-        self.weightsHiddenOutput = [
-            self.weightsHiddenOutput[j] - learningrate * d_output * hiddenLayerOutput[j]
-            for j in range(self.sizeofHidden)
-        ]
+        self.weightsHiddenOutput = [self.weightsHiddenOutput[j] - learningrate * d_output * hiddenLayerOutput[j] for j in range(self.sizeofHidden)]
         self.bias_output -= learningrate * d_output
 
         for i in range(self.sizeofInput):
             for j in range(self.sizeofHidden):
                 self.weights_input_hidden[i][j] -= learningrate * d_hidden_layer[j] * input[i]
+        for j in range(self.sizeofHidden):
             self.bias_hidden[j] -= learningrate * d_hidden_layer[j]
 
     def train(self, trainData, epochs, learningrate):
-        """func. to Train the neural network for specified numbers of epochs"""
+        """ trains the neural network over a specified number of epochs."""
         for epoch in range(epochs):
-            totaloss = 0
+            total_loss = 0
             for row in trainData:
                 input = list(map(float, row[:-1]))
                 actualOutput = float(row[-1])
-                predictedutput, hiddenLayerOutput = self.ForwardPropagation(input)
-                totaloss += (predictedutput - actualOutput) ** 2
-                self.BacwardPropagation(input, hiddenLayerOutput, predictedutput, actualOutput, learningrate)
-            print(f'Epoch {epoch + 1}/{epochs}, Loss: {totaloss / len(trainData)}')
+                predicted_output, hiddenLayerOutput = self.ForwardPropagation(input)
+                total_loss += (predicted_output - actualOutput) ** 2
+                self.BackwardPropagation(input, hiddenLayerOutput, predicted_output, actualOutput, learningrate)
+            print(f'Epoch {epoch + 1}/{epochs}, Loss: {total_loss / len(trainData)}')
 
-    
-    def predict(self, input, outputMin, outputMax):
-        """ a func. to return for the predicted output value for the given input"""
-        predictedutput, _ = self.ForwardPropagation(input)
-        return predictedutput * (outputMax - outputMin) + outputMin
+    def predict(self, inputs):
+        """predicts the output for a given input."""
+        if isinstance(inputs[0], list):
+            return [self.ForwardPropagation(row)[0] for row in inputs]
+        else:
+            return self.ForwardPropagation(inputs)[0]
